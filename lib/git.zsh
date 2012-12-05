a96b0c10903adbc4e9e24f390e5b3c7ed7c71e35
Brice Dutheil <brice.dutheil@gmail.com>
2012-12-05 20:06:14 +0100

    better way to get the  parse the git status (removed check on git 1.7.2)

--- lib/git.zsh
+++ lib/git.zsh
@@ -9,26 +9,24 @@ function git_prompt_info() {
 
 
 # Checks if working tree is dirty
-parse_git_dirty() {
+parse_git_dirty () {
   local SUBMODULE_SYNTAX=''
-  local GIT_STATUS=''
-  local CLEAN_MESSAGE='nothing to commit (working directory clean)'
-  if [[ "$(command git config --get oh-my-zsh.hide-status)" != "1" ]]; then
-    if [[ $POST_1_7_2_GIT -gt 0 ]]; then
-          SUBMODULE_SYNTAX="--ignore-submodules=dirty"
-    fi
-    if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" == "true" ]]; then
-        GIT_STATUS=$(command git status -s ${SUBMODULE_SYNTAX} -uno 2> /dev/null | tail -n1)
-    else
-        GIT_STATUS=$(command git status -s ${SUBMODULE_SYNTAX} 2> /dev/null | tail -n1)
-    fi
-    if [[ -n $GIT_STATUS ]]; then
-      echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
-    else
-      echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
-    fi
-  else
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+  if [[ $POST_1_7_2_GIT -gt 0 ]]; then
+    SUBMODULE_SYNTAX="--ignore-submodules=dirty"
+  fi
+  
+  local gitstat=$(git status $(SUBMODULE_SYNTAX) 2>/dev/null | grep '\(# Untracked\|# Changes\|# Changed but not updated:\)')
+
+  if [[ $(echo ${gitstat} | grep -c "^# Changes to be committed:$") > 0 ]]; then
+    echo -n "$ZSH_THEME_GIT_PROMPT_DIRTY"
+  fi
+
+  if [[ $(echo ${gitstat} | grep -c "^\(# Untracked files:\|# Changed but not updated:\)$") > 0 ]]; then
+    echo -n "$ZSH_THEME_GIT_PROMPT_UNTRACKED"
+  fi 
+
+  if [[ $(echo ${gitstat} | wc -l | tr -d ' ') == 0 ]]; then
+    echo -n "$ZSH_THEME_GIT_PROMPT_CLEAN"
   fi
 }
 
